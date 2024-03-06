920e89b09683bc2081d6c3f1a95f3d05319f4fb2
Deus Paul <paulfurlan@circuitzero.com>
2024-03-06 03:48:36 -0800

    fixed unit test

--- __test__/appOperation.test.js
+++ __test__/appOperation.test.js
@@ -7,3 +7,7 @@ test("multiplication property of zero", ()=> {
 test("adding two values", ()=> {
     expect(appOperations.add(5,5)).toBe(10);
 });
+
+test("test subtraction", ()=> {
+    expect(appOperations.subtract(15,5)).toBe(10);
+});
